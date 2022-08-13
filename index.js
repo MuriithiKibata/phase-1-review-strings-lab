f52dc7a1134a1526e1266e588524bf8a311d5277
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-13 22:53:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "ALICE"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//const upperCaseCurrentUser = currentUser.toUpperCase
+//const upperCaseCurrentUser = currentUser.toUpperCase
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+const shortGreeting = "Welcome, Alice!"
